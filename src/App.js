ce0cd68a4453357da9d8cc2abb691f3d6745aafb
Anestis Giannakosian <66698522+anegian@users.noreply.github.com>
2024-01-18 00:42:42 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -20,8 +26,28 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item(props) {
+  return (
+    <li>
+      <span style={props.item.packed ? { textDecoration: "line-through" } : {}}>
+        {props.item.quantity} {props.item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
