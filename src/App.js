b1afd69d1ae1c3e40094b7f6416d04b90f4a6498
Anestis Giannakosian <66698522+anegian@users.noreply.github.com>
2024-01-25 21:38:37 +0200

    updated "controlled elements"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,10 +21,48 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  // step a) create piece of state for the input
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    // prevent or else the page will be reset or re-rendered
+    e.preventDefault();
+
+    // if empty string nothing happens
+    if (!description) return;
+
+    const newItem = { id: Date.now(), quantity, description, packed: false };
+    console.log(newItem);
+
+    // return to initial states
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      {/* step b) use the state as value*/}
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        // step c) connect the state and the value using onChange handler
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +70,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
